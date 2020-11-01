06c1b328f0d264b147022b83b2a7b36e2df49957
Eloïse Schmitt <eloiseschmitt@iMac-de-Eloise.local>
2020-11-01 11:53:47 +0100

    modification store pour faire les appels api dedans et afficher les noms et nombre d'article en sidebar

--- src/components/sidebar/liste.vue
+++ src/components/sidebar/liste.vue
@@ -11,55 +11,44 @@
     </b-list-group>
 
     <h3>Espace</h3>
-    <b-list-group>
-      <b-list-group-item
+    <b-list-group v-for="(category, i) in getTypeAndNumbers" :key="i">
+        <b-list-group-item
         class="d-flex justify-content-between align-items-center"
         v-on:click="changeCat('Galaxie')"
       >
-        Galaxie
-        <b-badge variant="primary" pill>2</b-badge>
-      </b-list-group-item>
-      <b-list-group-item
-        class="d-flex justify-content-between align-items-center"
-        v-on:click="changeCat('Hubble')"
-      >
-        Hubble
-        <b-badge variant="primary" pill>2</b-badge>
-      </b-list-group-item>
-      <b-list-group-item
-        class="d-flex justify-content-between align-items-center"
-        v-on:click="changeCat('Jupiter')"
-      >
-        Jupiter
-        <b-badge variant="primary" pill>2</b-badge>
-      </b-list-group-item>
-      <b-list-group-item
-        class="d-flex justify-content-between align-items-center"
-        v-on:click="changeCat('Saturne')"
-      >
-        Saturne
-        <b-badge variant="primary" pill>2</b-badge>
-      </b-list-group-item>
-      <b-list-group-item
-        class="d-flex justify-content-between align-items-center"
-        v-on:click="changeCat('Univers')"
-      >
-        Univers
-        <b-badge variant="primary" pill>2</b-badge>
+       {{ category.name[0] }}
+        <b-badge variant="primary" pill>{{ category.numberOfArticles }}</b-badge>
       </b-list-group-item>
     </b-list-group>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
   name: "liste",
+  data() {
+    return {
+      categories: {
+        name: '',
+        numberOfArticles: ''
+      }
+    }
+  },
+  mounted() {
+    this.$store.dispatch('getArticles');
+  },
+  computed: {
+    ...mapGetters([
+      'getTypeAndNumbers'
+    ])
+  },
   methods: {
     changeCat(categorie) {
       this.$store.commit("changeCat", categorie);
     }
-  }
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
+let parseString = require('xml2js').parseString;
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        cat: 'Galaxie',
-        url: 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500446',
+        // C'est bon, range tes urls dans un autre fichier, sale sale !
+        urlsList: ['https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500446', 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500732',
+            'https://www.google.fr/alerts/feeds/02845282839133512907/15869804725181167392', 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290499193',
+            'https://www.google.fr/alerts/feeds/02845282839133512907/10919938935469249528', 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500446'],
+        categories: [],
+
+    },
+    getters: {
+        getTypeAndNumbers: state => {
+            return state.categories;
+        }
     },
     mutations: {
-        changeCat(state, newCat) {
-            state.cat = newCat;
-            switch (newCat) {
-                case 'Galaxie':
-                    state.url = 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500446';
-                    break;
-                case 'Hubble':
-                    state.url = 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500732';
-                    break;
-                case 'Jupiter':
-                    state.url = 'https://www.google.fr/alerts/feeds/02845282839133512907/15869804725181167392';
-                    break;
-                case 'Saturne':
-                    state.url = 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290499193';
-                    break;
-                case 'Univers':
-                    state.url = 'https://www.google.fr/alerts/feeds/02845282839133512907/10919938935469249528';
-                    break;
-                default:
-                    state.url = 'https://www.google.fr/alerts/feeds/02845282839133512907/6194519089290500446';
+        setArticles(state, payload) {
+            let category = {
+                name: payload.feed.title,
+                numberOfArticles: payload.feed.entry.length
             }
+            state.categories.push(category);
+        }
+    },
+    actions: {
+        getArticles: context => {
+            context.state.urlsList.forEach(url => {
+                axios
+                    .get(url)
+                    .then(response => {
+                        parseString(response.data, function (err, result) {
+                            context.commit('setArticles', result)
+                        })
+
+                    })
+                    .catch(e => {
+                        console.log(e)
+                        context.commit('setArticles', null)
+                    })
+            })
+
         }
     }
 })
